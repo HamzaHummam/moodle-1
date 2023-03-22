0a77d1970f21b0d5109ca933c383b0ba53226299
Hamza Hummam <hamzahummam16@gmail.com>
2023-03-22 10:38:35 +0500

    custom changes

---
+++ admin/tool/userprivileges/lang/en/tool_userprivileges.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     tool_userprivileges
+ * @category    string
+ * @copyright   2021 Ahsan Gul <ahsan.gul@seecs.edu.pk>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Moodle Managers Privileges System';
+$string['step1'] = 'Step 1: Users';
+$string['step1_help'] = 'Step 1: Schema Uniform';
+$string['step2'] = 'Step 2: Insert Archived Data';
+$string['step2_help'] = 'Step 2: Insert Archived Data';
+$string['step3'] = 'Step 3: Delete Archived Data';
+$string['step3_help'] = 'Step 3: Delete Archived Data';
+$string['tool_userprivileges'] = 'Moodle Managers Privileges System';
